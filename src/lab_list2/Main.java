1cf45eef1b47ebe61b01bd7ccdd8b815043415a6
TTomaszPWR <280598@student.pwr.edu.pl>
2024-03-14 19:33:40 +0100

    update

--- src/lab_list2/Main.java
+++ src/lab_list2/Main.java
@@ -1,6 +1,8 @@
 package lab_list2;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Scanner;
 
 import static lab_list2.FileWriteRead.write;
 import static lab_list2.FileWriteRead.read;
@@ -8,17 +10,71 @@ import static lab_list2.FileWriteRead.read;
 public class Main {
     public static void main(String[] args) {
 
-        Worker[] workers = new Worker[4];
+        /*Worker[] workers = new Worker[4];
         workers[0] = new FullTimeWorker("Jan", "Kowalski", "Manager", 12345678901L, 5, Status.FULLTIME, 5000);
         workers[1] = new FullTimeWorker("Anna", "Nowak", "Secretary", 12345678902L, 3, Status.THREEFOURTHS, 3000);
         workers[2] = new HourlyWorker("Piotr", "Kowalczyk", "Driver", 12345678903L, 1, 160, 20);
-        workers[3] = new HourlyWorker("Katarzyna", "Wójcik", "Cleaner", 12345678904L, 2, 120, 15);
-        write(workers);
+        workers[3] = new HourlyWorker("Katarzyna", "Wójcik", "Cleaner", 12345678904L, 2, 120, 15);*/
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Worker> workers = new ArrayList<>();
+
+        boolean inputWorkers = true;
+        while(inputWorkers){
+            System.out.println("Enter name: ");
+            String name = scanner.nextLine();
+            System.out.println("Enter surname: ");
+            String surname = scanner.nextLine();
+            System.out.println("Enter position: ");
+            String position = scanner.nextLine();
+            System.out.println("Enter PESEL: ");
+            long pesel = scanner.nextLong();
+            System.out.println("Enter internship: ");
+            int internship = scanner.nextInt();
+            System.out.println("Enter type of worker (1 - FullTime, 2 - Hourly): ");
+            int type = scanner.nextInt();
+            if(type==1){
+                System.out.println("Enter status (1 - FULLTIME, 2 - THREEFOURTHS, 3 - HALF, 4 - QUARTER): ");
+                int status = scanner.nextInt();
+                System.out.println("Enter base salary: ");
+                double baseSalary = scanner.nextDouble();
+
+                workers.add(new FullTimeWorker(name, surname, position, pesel, internship, Status.values()[status-1], baseSalary));
+                break;
+            }
+            else if(type==2){
+                System.out.println("Enter hours: ");
+                int hours = scanner.nextInt();
+                System.out.println("Enter base hour salary: ");
+                double baseHourSalary = scanner.nextDouble();
+                workers.add(new HourlyWorker(name, surname, position, pesel, internship, hours, baseHourSalary));
+                break;
+            }
+            else{
+                System.out.println("Wrong type of worker. Try again.");
+            }
+            scanner.nextLine();
+            System.out.println("Do you want to add another worker? (yes/no)");
+            String answer = scanner.nextLine();
+
+            if(answer.equals("no")||answer.equals("No")||answer.equals("NO")){
+                inputWorkers = false;
+            }
+        }
+
+        Worker[] array = new Worker[workers.size()];
+        array = workers.toArray(array);
+
+        write(array);
         Worker[] readWorkers = read();
         Iterator<Worker> iterator =  new ArrayIterator(readWorkers);
+        System.out.println("-------------------------------------------------------------------------------------------------\n" +
+                           "|Name           |Surname        |Position       |PESEL          |Internship     |Salary         |\n" +
+                           "-------------------------------------------------------------------------------------------------");
         while (iterator.hasNext()) {
             iterator.next().show();
         }
+
 
     }
 }
--- src/lab_list2/Worker.java
+++ src/lab_list2/Worker.java
@@ -17,7 +17,13 @@ public abstract class Worker implements Serializable {
     public void show() { System.out.println(this); }
     @Override
     public String toString() {
-        return String.format("Worker{name='%s', surname='%s', position='%s', pesel=%d, internship=%d, salary=%d}",
-                name, surname, position, pesel, internship, salary());
+        int howManySpaces1 = 15 - name.length();
+        int howManySpaces2 = 15 - surname.length();
+        int howManySpaces3 = 15 - position.length();
+        int howManySpaces4 = 15 - String.valueOf(pesel).length();
+        int howManySpaces5 = 15 - String.valueOf(internship).length();
+        int howManySpaces6 = 15 - String.valueOf(salary()).length();
+        return String.format("|%s%s|%s%s|%s%s|%s%d|%s%d|%s%d|",
+                name," ".repeat(howManySpaces1), surname," ".repeat(howManySpaces2), position," ".repeat(howManySpaces3)," ".repeat(howManySpaces4), pesel," ".repeat(howManySpaces5), internship," ".repeat(howManySpaces6), salary());
     }
 }
